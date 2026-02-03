e2901e8ec597682079a9ec1b4f9a393191fc4587
grayfalcon666 <2017215354@qq.com>
2026-02-04 00:31:24 +0800

    docs:delete app.env,store&retrieve secret_key from doppler,fix testTransferTx logic

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,10 +15,6 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	config, err := util.LoadConfig("../..")
-	if err != nil {
-		log.Fatal("cannot load config:", err)
-	}
-
 	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
